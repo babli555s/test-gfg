64ee03cd8174c8b555e782e6fb844bb62b04cf6d
babli555s <141941554+babli555s@users.noreply.github.com>
2024-11-30 17:42:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -2,28 +2,25 @@
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            int N = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
             String input_line[] = read.readLine().trim().split("\\s+");
-            int arr[]= new int[N];
-            for(int i = 0; i < N; i++)
-                arr[i] = Integer.parseInt(input_line[i]);
+            int N = input_line.length;
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input_line[i]);
             int sum = Integer.parseInt(read.readLine());
 
             Solution ob = new Solution();
-            if(ob.isSubsetSum(N, arr, sum))
-            System.out.println(1);
+            if (ob.isSubsetSum(arr, sum))
+                System.out.println("true");
             else
-            System.out.println(0);
+                System.out.println("false");
 
-            
+            System.out.println("~");
         }
     }
 }
@@ -31,53 +28,28 @@ class GFG
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution{
+class Solution {
 
-
-    static Boolean isSubsetSum(int N, int arr[], int sum){
+    static Boolean isSubsetSum(int arr[], int target) {
         // code here
-       int n = arr.length;
-
-        // Initialize the previous row of the DP table
-        boolean prev[] = new boolean[sum + 1];
-        
-        // Initialize the first cell of the DP table
-        prev[0] = true;
-
-        // If the first element is less than or equal to k, mark that sum as achievable
-        if (arr[0] <= sum) {
-            prev[arr[0]] = true;
-        }
-
-        // Fill in the DP table using a bottom-up approach
-        for (int ind = 1; ind < n; ind++) {
-            // Create an array to store the current row of the DP table
-            boolean cur[] = new boolean[sum + 1];
-            
-            // Initialize the first column of the current row
-            cur[0] = true;
-            
-            for (int target = 1; target <= sum; target++) {
-                // Calculate if the current target can be achieved without taking the current element
-                boolean notTaken = prev[target];
-                
-                // Calculate if the current target can be achieved by taking the current element
-                boolean taken = false;
-                if (arr[ind] <= target) {
-                    taken = prev[target - arr[ind]];
-                }
-                
-                // Store the result in the current row of the DP table
-                cur[target] = notTaken || taken;
-            }
-            
-            // Update the previous row with the current row
-            prev = cur;
-        }
-
-        // The final result is stored in the last cell of the previous row
-        return prev[sum];
+        int n=arr.length;
+       boolean[][]dp=new boolean[n+1][target+1];
+       for(int i=0;i<=n;i++){
+           dp[i][0]=true;
+       }
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=target;j++){
+               if(arr[i-1]<=j){
+                   dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+               }
+               else{
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       }
+       return dp[n][target];
     }
+    
 }
