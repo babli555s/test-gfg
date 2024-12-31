aae177dc4ba250efc1d8f769668cf3a78e7fffdb
babli555s <141941554+babli555s@users.noreply.github.com>
2024-12-31 12:38:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        if(n==0 || n==1)
+        return 1;
+        int fact=1;
+        for(int i=2;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
