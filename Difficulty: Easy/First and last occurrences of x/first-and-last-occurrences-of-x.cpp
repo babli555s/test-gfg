d5a7c039be1f58ad09e4fbb075e2483dbb226141
babli555s <141941554+babli555s@users.noreply.github.com>
2025-01-01 19:16:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> firstAndLast(int x, vector<int> &arr) {
+        // Code here
+        int n=arr.size();
+        int first=-1,last=-1;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]==x) {
+            if(first==-1){
+                first=i;
+        }
+       
+          last=i;
+        
+        }
+        }
+        if(first==-1){
+            return {-1};
+        }
+        return {first,last};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        vector<int> ans = obj.firstAndLast(x, arr);
+        for (int i : ans) {
+            cout << i << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
