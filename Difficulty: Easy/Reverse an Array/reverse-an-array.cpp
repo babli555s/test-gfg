15a03c7ed07a42037cd6529bd2c77918dd4422a5
babli555s <141941554+babli555s@users.noreply.github.com>
2025-03-28 11:59:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     void reverseArray(vector<int> &arr) {
         // code here
         int n=arr.size();
         for(int i=0;i<n/2;i++){
-        swap(arr[i],arr[n-i-1]);
+            swap(arr[i],arr[n-i-1]);
         }
-        
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
