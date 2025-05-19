24f020c59d351303d0c9eccc82bbb738b7f4dac5
babli555s <141941554+babli555s@users.noreply.github.com>
2025-05-19 11:59:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.cpp
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 // } Driver Code Ends
 
-// User function template for C++
-
 class Solution {
   public:
     // Function to check if a string is a palindrome.
     bool isPalindrome(string& s) {
-        // code here
-        int n=s.length();
-        for(int i=0;i<=n/2;i++){
-            if(s[i]!=s[n-i-1])
-            return false;
+        // code her
+        int n=s.size();
+        for(int i=0;i<n/2;i++){
+            if(s[i]!=s[n-i-1]){
+                return false;
+            }
         }
         return true;
+        
     }
 };
 
