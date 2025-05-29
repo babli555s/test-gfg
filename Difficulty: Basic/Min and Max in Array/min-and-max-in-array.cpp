e36db31a7799385befb494e57dc99ed3f24bc11e
babli555s <141941554+babli555s@users.noreply.github.com>
2025-05-29 18:47:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
@@ -1,53 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
+  
+    int getmin(const vector<int>&arr){
+        int n=arr.size();
+        int res=arr[0];
+        for(int i=1;i<n;i++){
+            res=min(res,arr[i]);
+        }
+        return res;
+    }
+    int getmax(const vector<int>&arr){
+         int n=arr.size();
+        int res=arr[0];
+        for(int i=1;i<n;i++){
+            res=max(res,arr[i]);
+        }
+        return res;
+    }
     pair<int, int> getMinMax(vector<int> arr) {
         // code here
-        int  n=arr.size();
-        int max=arr[0];
-        int min=arr[0];
-        for(int i=0;i<n;i++){
-            if(arr[i]>max ){
-                max=arr[i];
-            }
-            if(arr[i]<min){
-                min=arr[i];
-            }
-            
-        }
-        return {min,max};
+        int minimum=getmin(arr);
+        int maximum=getmax(arr);
+        return{minimum,maximum};
+        
+       
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        pair<int, int> pp = ob.getMinMax(arr);
-        cout << pp.first << " " << pp.second << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
