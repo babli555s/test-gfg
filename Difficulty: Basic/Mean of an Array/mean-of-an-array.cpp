3275f16b76f3418e1f1d6fe7f6bb5c275430eec6
babli555s <141941554+babli555s@users.noreply.github.com>
2025-09-23 00:14:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Mean of an Array/mean-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int findMean(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int sum=0;
+        int mean=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        return sum/n;
+    }
+};
