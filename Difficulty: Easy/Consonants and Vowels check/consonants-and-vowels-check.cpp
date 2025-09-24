4c6d2bd423aa637f6c1aab5cb470f648afb54c05
babli555s <141941554+babli555s@users.noreply.github.com>
2025-09-24 14:48:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Consonants and Vowels check/consonants-and-vowels-check.cpp
+++ Difficulty: Easy/Consonants and Vowels check/consonants-and-vowels-check.cpp
@@ -1,69 +1,26 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void checkString(string s);
-
-
-// } Driver Code Ends
-
-//User function Template for C++
-
-void checkString(string s)
-{
-     int v=0;
-    int c=0;
-    for(char ch:s){
-        if(isalpha(ch)){
-        if(ch=='a'||ch=='e'|| ch=='i'|| ch=='o' || ch=='u'){
-        v++;
-        }
-        else{
-            c++;
+void checkString(string s) {
+    int v = 0; // vowels
+    int c = 0; // consonants
+    int n = s.size();
+    for (int i = 0; i < n; i++) {
+        char ch = s[i];
+        if (isalpha(ch)) { // only consider alphabets
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                v++;
+            } else {
+                c++;
+            }
         }
     }
-    }
-   
-    
-    //Your code here
-    
-    if(v>c){
-        cout<<"Yes";
-    }
-    else if(c>v){
-        cout<<"No";
-    }
-    else{
-        cout<<"Same";
-    }
-    
-    cout<<endl;
+
+    if (v > c)
+        cout << "Yes";
+    else if (c > v)
+        cout << "No";
+    else
+        cout << "Same";
+
+    cout << endl;
 }
 
-
-
-//{ Driver Code Starts.
-
-int main() {
-	int t;
-	cin>>t;
-	cin.ignore();
-	while(t--)
-	{
-	    
-	    string s;
-	    getline(cin,s);
-	    
-	    //function call
-	    checkString(s);
-	   
-	
-cout << "~" << "\n";
-}
-	return 0;
-}
-
-
-// } Driver Code Ends
