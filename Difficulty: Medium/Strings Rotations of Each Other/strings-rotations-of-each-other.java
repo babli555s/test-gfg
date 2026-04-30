ee7e14a881a58b41e3aca861c711ba55aa607eed
babli555s <141941554+babli555s@users.noreply.github.com>
2026-04-30 18:06:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean areRotations(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        String temp = s1 + s1;
+        return kmpSearch(temp, s2);
+    }
+
+    private boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPS(pattern);
+        int i = 0, j = 0;
+        while (i < text.length()) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+                if (j == pattern.length()) {
+                    return true; // found match
+                }
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int[] computeLPS(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int len = 0, i = 1;
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
